c2d3222178786659ff89c51777b9150b8d6fc9d3
Colombianitov2 <82978437+colombianitov2@users.noreply.github.com>
2026-06-12 03:11:15 -0500

    Agregar punto de entrada WPF

---
+++ src/CartaPsicometrica/Program.cs
@@ -0,0 +1,10 @@
+namespace CartaPsicometrica;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var app = new System.Windows.Application();
+        app.Run(new MainWindow());
+    }
+}
